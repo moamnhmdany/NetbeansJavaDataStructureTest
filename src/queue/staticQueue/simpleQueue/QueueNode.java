a440c8be5eb330e34c0796d642b4772ece647e7f
Lenovo <Lenovo@moamnPc>
2024-08-27 13:30:35 +0300

    queue types

---
+++ src/queue/staticQueue/simpleQueue/QueueNode.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package queue.staticQueue.simpleQueue;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class QueueNode {
+    
+    int[] queur ;
+    int front ;
+    int rear ;
+    int data ;
+    int size ;
+    
+    
+    public void enqueue(int data){
+    
+    
+    }
+    
+    
+     public void dequeue(){
+     
+     }
+     
+    public void isEmpty(){
+    
+    }
+     public void isFull(){
+     
+     }
+      public void peek(){
+      
+      }
+      public void display(){
+      
+      }
+
+
+
+
+
+
+
+
+
+
+
+}

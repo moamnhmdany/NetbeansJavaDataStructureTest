9d4856b835b18dcd39aa3e3a5430e1b9c7cd8113
Lenovo <Lenovo@moamnPc>
2024-10-13 23:29:58 +0300

    done make static leaner queue 222

--- src/queue/staticQueue/simpleQueue/LinearQueue.java
+++ src/queue/staticQueue/simpleQueue/LinearQueue.java
@@ -12,6 +12,67 @@ package queue.staticQueue.simpleQueue;
 public class LinearQueue {
     
     
+     int[] queue ;
+    int front ;
+    int rear = 0 ;
+    int data ;
+    int size = 0 ;
+    int counter = 0;
     
+    public LinearQueue(int size){
+    
+        this.queue = new int[size];
+        this.size = size ;
+        this.front = 0 ;
+    }    
+    public void enqueue(int data){
+          if(isFull()){
+              System.out.println("the queue is full please dequeue");
+          }else{
+        this.queue[this.rear]= data ;
+        this.rear++;
+        //
+          }
+    }
+    
+    
+     public void dequeue(){
+        if(isEmpty()){
+            System.out.println("the queue is empty please add new element");
+             System.exit(-1);
+        }else{
+        
+            for(int index = 0 ; index < this.rear-1; index++){
+               
+               this.queue[index] = this.queue[index++]; 
+            }
+          this.queue[this.rear-1] = 0 ;   
+          this.rear-- ;
+        }
+     }
+     
+    public boolean isEmpty(){
+      return this.rear == 0 ;
+    }
+    
+     public boolean isFull(){
+       return this.queue.length == this.rear ;
+     }
+      public int peek(){
+          if(isEmpty()){
+            System.out.println("the queue is empty");
+            System.exit(-1);
+          }
+         
+         return this.queue[this.front];
+      }
+      
+      public void display(){
+         for(int index = 0 ; index < this.rear ; index++){
+         
+             System.out.print(" <== "+"item "+this.queue[index] +" ");
+         }
+      }
+
     
 }

fc12256ff3e90366e87815ca9de348d4a1cff201
Lenovo <Lenovo@moamnPc>
2024-10-13 23:31:09 +0300

    done make static leaner queue 333

--- src/queue/staticQueue/simpleQueue/QueueNode.java
+++ src/queue/staticQueue/simpleQueue/QueueNode.java
@@ -11,44 +11,4 @@ package queue.staticQueue.simpleQueue;
  */
 public class QueueNode {
     
-    int[] queur ;
-    int front ;
-    int rear ;
-    int data ;
-    int size ;
-    
-    
-    public void enqueue(int data){
-    
-    
-    }
-    
-    
-     public void dequeue(){
-     
-     }
-     
-    public void isEmpty(){
-    
-    }
-     public void isFull(){
-     
-     }
-      public void peek(){
-      
-      }
-      public void display(){
-      
-      }
-
-
-
-
-
-
-
-
-
-
-
 }
